cc3467b6aeee2108707ce6e8b7c93f93dab2c1a7
Claire Dune <claire.dune@univ-tln.fr>
2025-02-22 22:56:21 +0100

    Update test_quaternion.py

--- test_quaternion.py
+++ test_quaternion.py
@@ -6,31 +6,32 @@ import time
 import cldLib as cld
 
 # Création de la matrice de transformation
-wMo = cld.homogeneousMatrix(0, 0, 0, 0, 0, 0)  # Ex. rotation + translation
-print("pose 0, 0, 0, 10, 20, 30")
+pose = np.array([0, 0, 0, 10, 20, 30])
+wMo = cld.homogeneousMatrix(pose[0], pose[1], pose[2], pose[3], pose[4], pose[5])  # Ex. rotation + translation
+print("pose", pose)
 print("Matrice de la pose :\n", wMo)
 
 t_wo,euler_wo = cld.matrix_to_translation_euler(wMo);
 print("Translation et angles d'Euler :\n", t_wo, euler_wo)
 
 
-# ---- on definiti la vitesse de commande dans le repere du robot----#
+# ---- on definiti la vitesse de commande dans le repère du robot----#
 v0=np.array([1.0,0.0,0.0])
 w0=np.array([0.0,0.0,0.0])
 
 # --- on deduit le deplacement du robot si on applique cette vitesse  pendant dt secondes---#
-dt=1
+dt=0.1
 v=np.copy(v0)
 w=np.copy(w0)
 
-o2Mo = cld.homogeneous_from_twist(w, v, dt)
-print("Matrice homogène du déplacement :\n", o2Mo)
-t_oo2,euler_oo2 = cld.matrix_to_translation_euler(o2Mo);
+oMo2 = cld.homogeneous_from_twist(w, v, dt)
+print("Matrice homogène du déplacement :\n", oMo2)
+t_oo2,euler_oo2 = cld.matrix_to_translation_euler(oMo2);
 print("Translation et angles d'Euler du mouvement :\n", t_oo2, euler_oo2)
 
 
 
-wMo2 = wMo @ np.linalg.inv(o2Mo)
+wMo2 = wMo @ oMo2
 print("Matrice homogène apres déplacement :\n", wMo2)
 
 t_wo2,euler_wo2 = cld.matrix_to_translation_euler(wMo2);
